bfeb2fca1608d2a5c6fdd8f96b4850e88d486857
Andre Kolber <andrekolber1@gmail.com>
2021-12-31 11:07:13 -0500

    added async/await keywords

--- app.js
+++ app.js
@@ -1,114 +1,109 @@
 // Part 1: Number Facts
 const numberURL = 'http://numbersapi.com';
+
 // 1.
-axios.get(`${numberURL}/4?json`).then((favNum) => {
-	console.log(favNum.data);
-});
+async function getFavNumber() {
+	let res = await axios.get(`${numberURL}/4?json`);
+	console.log(res.data.text);
+}
 
 // 2.
-fact1 = document.getElementById('fact1');
-fact2 = document.getElementById('fact2');
-fact3 = document.getElementById('fact3');
+async function numbersData() {
+	fact1 = document.getElementById('fact1');
+	fact2 = document.getElementById('fact2');
+	fact3 = document.getElementById('fact3');
 
-numFacts = axios
-	.get(`${numberURL}/1`)
-	.then((num1) => {
-		fact1.innerHTML = num1.data;
-		return axios.get(`${numberURL}/2`);
-	})
-	.then((num2) => {
-		fact2.innerHTML = num2.data;
-		return axios.get(`${numberURL}/3`);
-	})
-	.then((num3) => {
-		fact3.innerHTML = num3.data;
-	})
-	.catch((err) => console.log(err));
+	let fact1Promise = axios.get(`${numberURL}/1`);
+	let fact2Promise = axios.get(`${numberURL}/2`);
+	let fact3Promise = axios.get(`${numberURL}/3`);
+
+	let fact1data = await fact1Promise;
+	let fact2data = await fact2Promise;
+	let fact3data = await fact3Promise;
+
+	fact1.innerHTML = fact1data.data;
+	fact2.innerHTML = fact2data.data;
+	fact3.innerHTML = fact3data.data;
+}
+numbersData();
 
 // 3.
-favNumFactsList = document.getElementById('favNumFacts');
 
-axios
-	.get(`${numberURL}/4`)
-	.then((fact1) => {
-		const favNumFact1 = document.createElement('li');
-		console.log(fact1);
-		favNumFact1.innerHTML = fact1.data;
-		favNumFactsList.append(favNumFact1);
-		return axios.get(`${numberURL}/4`);
-	})
-	.then((fact2) => {
-		const favNumFact2 = document.createElement('li');
-		console.log(fact2);
-		favNumFact2.innerHTML = fact2.data;
-		favNumFactsList.append(favNumFact2);
-		return axios.get(`${numberURL}/4`);
-	})
-	.then((fact3) => {
-		const favNumFact3 = document.createElement('li');
-		console.log(fact3);
-		favNumFact3.innerHTML = fact3.data;
-		favNumFactsList.append(favNumFact3);
-		return axios.get(`${numberURL}/4`);
-	})
-	.then((fact4) => {
-		const favNumFact4 = document.createElement('li');
-		console.log(fact4);
-		favNumFact4.innerHTML = fact4.data;
-		favNumFactsList.append(favNumFact4);
-	})
-	.catch((err) => console.log(err));
+async function favNumberFacts() {
+	favNumFactsList = document.getElementById('favNumFacts');
+	const favNumFact1 = document.createElement('li');
+	const favNumFact2 = document.createElement('li');
+	const favNumFact3 = document.createElement('li');
+	const favNumFact4 = document.createElement('li');
+
+	let fact1Promise = axios.get(`${numberURL}/4`);
+	let fact2Promise = axios.get(`${numberURL}/4`);
+	let fact3Promise = axios.get(`${numberURL}/4`);
+	let fact4Promise = axios.get(`${numberURL}/4`);
+
+	let fact1data = await fact1Promise;
+	let fact2data = await fact2Promise;
+	let fact3data = await fact3Promise;
+	let fact4data = await fact4Promise;
+
+	favNumFact1.innerHTML = fact1data.data;
+	favNumFact2.innerHTML = fact2data.data;
+	favNumFact3.innerHTML = fact3data.data;
+	favNumFact4.innerHTML = fact4data.data;
+
+	favNumFactsList.append(favNumFact1, favNumFact2, favNumFact3, favNumFact4);
+}
+favNumberFacts();
 
 // Part 2: Deck of Cards
+const cardsAPI = 'https://deckofcardsapi.com/api/deck';
 
 // 1.
-cardsAPI = 'https://deckofcardsapi.com/api/deck';
-
-axios
-	.get(`${cardsAPI}/new/draw/?count=1`)
-	.then((data) => {
-		let { suit, value } = data.data.cards[0];
-		console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
-	})
-	.catch((err) => console.log(err));
+async function requestCard() {
+	let res = await axios.get(`${cardsAPI}/new/draw/?count=1`);
+	let value = res.data.cards[0].value;
+	let suit = res.data.cards[0].suit;
+	console.log(`${value} of ${suit}`);
+}
+requestCard();
 
 // 2.
-let firstCard = null;
-axios
-	.get(`${cardsAPI}/new/draw/`)
-	.then((data) => {
-		console.log(data);
-		firstCard = data.data.cards[0];
-		let deckId = data.data.deck_id;
-		return axios.get(`${cardsAPI}/${deckId}/draw/`);
-	})
-	.then((data) => {
-		let secondCard = data.data.cards[0];
-		[ firstCard, secondCard ].forEach(function(card) {
-			console.log(`${card.value.toLowerCase()} of ${card.suit.toLowerCase()}`);
-		});
+async function requestFromSameDeck() {
+	let firstCard = null;
+	let res = await axios.get(`${cardsAPI}/new/draw/`);
+	firstCard = res.data.cards[0];
+	let deckId = res.data.deck_id;
+
+	let res2 = await axios.get(`${cardsAPI}/${deckId}/draw/`);
+	let secondCard = res2.data.cards[0];
+	[ firstCard, secondCard ].forEach(function(card) {
+		console.log(`${card.value.toLowerCase()} of ${card.suit.toLowerCase()}`);
 	});
+}
+requestFromSameDeck();
 
 // // 3.
-
 let deckId = null;
-const drawBtn = document.getElementById('draw-a-card');
 const cardArea = document.getElementById('cards-area');
+const drawBtn = document.getElementById('draw-a-card');
+const msg = document.getElementById('msg');
+drawBtn.style.display = 'block';
 
-axios.get(`${cardsAPI}/new/shuffle/`).then((data) => {
-	deckId = data.data.deck_id;
-	drawBtn.style.display = 'block';
-});
+async function newDeck() {
+	let res = await axios.get(`${cardsAPI}/new/shuffle/`);
+	deckId = res.data.deck_id;
+}
+newDeck();
 
-drawBtn.addEventListener('click', function() {
-	axios.get(`${cardsAPI}/${deckId}/draw/`).then((data) => {
-		let cardsSrc = data.data.cards[0].image;
-		let cardImg = document.createElement('img');
-		cardImg.setAttribute('src', cardsSrc);
-		cardArea.append(cardImg);
+drawBtn.addEventListener('click', async function() {
+	let res = await axios.get(`${cardsAPI}/${deckId}/draw/`);
+	let cardsSrc = res.data.cards[0].image;
+	let cardImg = document.createElement('img');
+	cardImg.setAttribute('src', cardsSrc);
+	cardArea.append(cardImg);
 
-		if (data.data.remaining === 0) {
-			drawBtn.style.display = 'none';
-		}
-	});
+	if (res.data.remaining === 0) {
+		drawBtn.style.display = 'none';
+		msg.innerHTML = 'End of Deck. Refresh to draw again.';
+	}
 });
